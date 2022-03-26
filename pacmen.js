be2f2a83cb325036ec989e7967229f81e83ea140
zkm5022 <97204258+zkm5022@users.noreply.github.com>
2022-03-26 12:15:00 -0400

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['PacMan1.png', 'PacMan2.png'],
+  ['PacMan3.png', 'PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
